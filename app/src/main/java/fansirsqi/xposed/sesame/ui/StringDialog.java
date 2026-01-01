68c3767f55a7c2310cac6cd4ff8c2911d1f75f38
sbtang17 <sbtang627@gmail.com>
2026-01-02 00:06:55 +0800

    尝试去掉警告

--- app/src/main/java/fansirsqi/xposed/sesame/ui/StringDialog.java
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/StringDialog.java
@@ -119,7 +119,7 @@ public class StringDialog {
                 .setPositiveButton(positiveButton, (dialog, which) -> dialog.dismiss())
                 .create();
 
-        alertDialog.show();
+        // alertDialog.show();
 
         Button button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
         if (button != null) {
